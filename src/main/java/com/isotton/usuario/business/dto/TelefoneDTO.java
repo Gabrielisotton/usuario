1d00fdf2f3f0b4f1c42657030778484d4c798a7d
Gabriel <gabi-feltrin@hotmail.com>
2025-08-19 19:54:00 -0300

    commit inicial

---
+++ src/main/java/com/isotton/usuario/business/dto/TelefoneDTO.java
@@ -0,0 +1,15 @@
+package com.isotton.usuario.business.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TelefoneDTO {
+
+    private Long id;
+    private String numero;
+    private String ddd;
+}
---
+++ src/main/java/com/isotton/usuario/infrastructure/entity/telefone.java
@@ -0,0 +1,27 @@
+package com.isotton.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Telefone")
+@Builder
+
+public class Telefone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "numero", length = 10)
+    private String numero;
+    @Column(name = "ddd", length = 3)
+    private String ddd;
+    @Column(name = "usuario_id")
+    private Long usuario_id;
+
+
+}
